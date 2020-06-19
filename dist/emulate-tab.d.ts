2f3751b5fe7a91abd0897fe2435eca974c222d34
Emanuel Hein <github@e-hein.de>
2020-06-19 21:20:56 +0200

    add pre-build dist results

---
+++ dist/emulate-tab.d.ts
@@ -0,0 +1,16 @@
+export declare function emulateTab(): Promise<void>;
+export declare namespace emulateTab {
+    const from: typeof emulateTabFrom;
+    const to: (target: HTMLElement) => Promise<void>;
+    const toPreviousElement: () => Promise<void>;
+    const toNextElement: () => Promise<void>;
+    const backwards: () => Promise<void>;
+}
+declare function emulateTabFrom(source?: HTMLElement): {
+    toPreviousElement: () => Promise<void>;
+    backwards: () => Promise<void>;
+    to: (target: HTMLElement) => Promise<void>;
+    toNextElement: () => Promise<void>;
+};
+export declare function findAllElementsSelectableByTab(): HTMLElement[];
+export {};
